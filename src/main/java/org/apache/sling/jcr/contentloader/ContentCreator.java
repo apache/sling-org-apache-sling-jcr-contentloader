e3aee7573cd271a2accd4768051c04361406be1d
Eric Norman <enorman@apache.org>
2022-12-01 16:35:46 -0800

    SLING-11713 Change ACL json input structure
    to be less ambiguous for restrictions

--- src/main/java/org/apache/sling/jcr/contentloader/ContentCreator.java
+++ src/main/java/org/apache/sling/jcr/contentloader/ContentCreator.java
@@ -19,6 +19,7 @@
 package org.apache.sling.jcr.contentloader;
 
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
@@ -203,6 +204,28 @@ public interface ContentCreator {
     	throw new UnsupportedOperationException();
 	}
     
+    /**
+     * Creates an Access Control Entry for the current node for the specified
+     * principal and privileges.
+     *
+     * @param principal         the user or group id for the ACE
+     * @param privileges        the set of privileges to allow or deny for the principal
+     * @param order             specifies the position of the ACE in the containing ACL. (may be null)
+     *                          Value should be one of these:
+     *                          <table>
+     *                          <caption>Values</caption>
+     *                          <tr><td>first</td><td>Place the target ACE as the first amongst its siblings</td></tr>
+     *                          <tr><td>last</td><td>Place the target ACE as the last amongst its siblings</td></tr>
+     *                          <tr><td>before xyz</td><td>Place the target ACE immediately before the sibling whose name is xyz</td></tr>
+     *                          <tr><td>after xyz</td><td>Place the target ACE immediately after the sibling whose name is xyz</td></tr>
+     *                          <tr><td>numeric</td><td>Place the target ACE at the specified index</td></tr>
+     *                          </table>
+     * @throws RepositoryException If anything goes wrong.
+     */
+    default void createAce(String principal, Collection<LocalPrivilege> privileges, String order) throws RepositoryException {
+        throw new UnsupportedOperationException();
+    }
+
     /**
      * Gets the current parent Node
      * @return the current parent node or null
